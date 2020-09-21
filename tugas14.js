d01cdbc47072b4df855400a717f1ca57ec33c8e8
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-21 09:24:52 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+var tgl = 37 / 11
+
+console.log("Hasil dari 37/11 : ", tgl)
+
+console.log("Hasil dari math round :", Math.round(tgl))
+console.log("Hasil dari math ceil :",Math.ceil(tgl))
+console.log("Hasil dari math floor :",Math.floor(tgl))
+
